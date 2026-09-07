a94c9bcc20a7b97800697b4755c1f6a91b0a4e83
NoelPadronDiaz <30351524+NoelPadronDiaz@users.noreply.github.com>
2026-09-07 16:57:21 +0100

    feat: add API endpoints for player CRUD operations

--- api/players/[id].ts
+++ api/players/[id].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { PLAYER_COLUMNS, sql, toDto, type PlayerRow } from '../_db';
+import { PLAYER_COLUMNS, sql, toDto, type PlayerRow } from '../_db.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { id } = req.query;
